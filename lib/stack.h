bc5bae2c4a03b2983e20b19033d59d119a2d0ad0
alec-bell <alec.bell@snowball.software>
2020-05-17 14:22:27 -0400

    Move stack method implementation to header file

--- lib/stack.h
+++ lib/stack.h
@@ -19,4 +19,28 @@ namespace ds
     private:
         node<T>* head;
     };
+
+    template<typename T>
+    T stack<T>::pop()
+    {
+        T data = head->data;
+        head = head->next;
+        return data;
+    }
+
+    template<typename T>
+    T stack<T>::peek()
+    {
+        return head->data;
+    }
+
+    template<typename T>
+    void stack<T>::push(T data)
+    {
+        node<T>* new_node = new node<T>();
+        new_node->data = data;
+        new_node->next = head;
+        head = new_node;
+    }
+
 }
